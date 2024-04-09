89bb5ed2247a8ca1e94b84915d7e2dacb7087b16
Tony Kuosa <tony.kuosa@helsinki.fi>
2024-04-09 16:03:44 +0300

    toteutettu invoke-tehtävät start, test, coverage-report

---
+++ budjetointisovellus/tasks.py
@@ -0,0 +1,22 @@
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/budget_text_ui.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=True)
+
+from subprocess import call
+from sys import platform
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
+    if platform != "win32":
+        call(("xdg-open", "htmlcov/index.html"))
